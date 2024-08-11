8348156f90e4031b1d2e1a19f367417418229018
gtchappy <2425172709@qq.com>
2024-08-11 10:04:15 +0800

    complete the icons in the navigation bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+require("icons/money.svg")
+require("icons/tag.svg")
+require("icons/chart.svg")
 
 const NavWrapper = styled.div`
     //border: 1px blue solid;
     line-height: 24px;
-    box-shadow: 0 0 3px rgba(0,0,0,0.25);
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
 
     > ul {
         display: flex;
@@ -14,22 +17,40 @@ const NavWrapper = styled.div`
         > li {
             flex: 1; /* 每个列表项占据可用空间 */
             text-align: center; /* 文本居中 */
-            padding: 16px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 4px 0;
+
+            > .icon {
+                width: 24px;
+                height: 24px;
+            }
         }
     }
 `
 
-function Nav(){
+function Nav() {
     return <NavWrapper>
         <ul>
             <li>
-                <Link to="/tags">tags</Link>
+                <svg className="icon" fill="red">
+                    <use href="#tag"></use>
+                </svg>
+                <Link to="/tags">标签页</Link>
             </li>
             <li>
-                <Link to="/money">money</Link>
+                <svg className="icon" fill="red">
+                    <use href="#money"></use>
+                </svg>
+                <Link to="/money">记账页</Link>
             </li>
             <li>
-                <Link to="/statistics">statistics</Link>
+                <svg className="icon" fill="red">
+                    <use href="#chart"></use>
+                </svg>
+                <Link to="/statistics">统计页</Link>
             </li>
         </ul>
     </NavWrapper>
