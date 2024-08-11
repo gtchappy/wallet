dfb52ba89463c0fa07ba6a9d0a175788a89bce8d
gtchappy <2425172709@qq.com>
2024-08-11 08:38:08 +0800

    complete the navigation bar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,74 @@
-import React from 'react';
+import React from "react";
+import {Routes, Route, Link, Navigate, HashRouter} from "react-router-dom";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+    border: 1px red solid;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`
+const Main = styled.div`
+    border: 1px green solid;
+    flex-grow: 1;
+    overflow: auto;
+`
+const Nav = styled.div`
+    border: 1px blue solid;
+
+    > ul {
+        display: flex;
+        justify-content: space-between;
+
+        > li {
+            flex: 1; /* 每个列表项占据可用空间 */
+            text-align: center; /* 文本居中 */
+            padding: 16px;
+        }
+    }
+`
 
 function App() {
     return (
-        <div>
-            hello
-        </div>
+        <HashRouter basename="/">
+            <Wrapper>
+                <Main>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/money"/>}/>
+                        <Route path="/tags" element={<Tags/>}/>
+                        <Route path="/money" element={<Money/>}/>
+                        <Route path="/statistics" element={<Statistics/>}/>
+                        <Route path="*" element={<Navigate to="/money"/>}/>
+                    </Routes>
+                </Main>
+                <Nav>
+                    <ul>
+                        <li>
+                            <Link to="/tags">tags</Link>
+                        </li>
+                        <li>
+                            <Link to="/money">money</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">statistics</Link>
+                        </li>
+                    </ul>
+                </Nav>
+            </Wrapper>
+        </HashRouter>
     );
 }
 
-export default App;
+function Statistics() {
+    return <h2>Statistics</h2>;
+}
+
+function Tags() {
+    return <h2>Tags</h2>;
+}
+
+function Money() {
+    return <h2>Money</h2>;
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import  './index.scss'
 import reportWebVitals from './reportWebVitals';
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
